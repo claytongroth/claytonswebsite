66dfe353adf257638ced07655f62396883cd05ce
claytongroth <claytoncgrth@gmail.com>
2020-03-15 15:24:03 -0600

    Updates

--- precache-manifest.6ff7b55fe2a1645605b4e7f3b150444d.js
+++ precache-manifest.6ff7b55fe2a1645605b4e7f3b150444d.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "528b9989df2ff412d2b1d66be3049ccc",
+    "revision": "da01018072453f3560461b1203613846",
     "url": "/claytonswebsite/index.html"
   },
   {
-    "revision": "25835c91ce84a5d97c87",
+    "revision": "9018fcf6693d54931c50",
     "url": "/claytonswebsite/static/css/main.dc29349c.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/claytonswebsite/static/js/2.1e8014bb.chunk.js"
   },
   {
-    "revision": "25835c91ce84a5d97c87",
-    "url": "/claytonswebsite/static/js/main.be8d11fa.chunk.js"
+    "revision": "9018fcf6693d54931c50",
+    "url": "/claytonswebsite/static/js/main.184ef1e6.chunk.js"
   },
   {
     "revision": "6d6d21c48a4fb70e6368",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/claytonswebsite/precache-manifest.c14596697d2105f458e75a1abc7fa5f6.js"
+  "/claytonswebsite/precache-manifest.6ff7b55fe2a1645605b4e7f3b150444d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/claytonswebsite/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
